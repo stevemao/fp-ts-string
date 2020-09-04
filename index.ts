6ce8b1e6005d08a157854497ed10cd8d9e0fdfae
Steve Mao <maochenyan@gmail.com>
2020-09-04 20:36:07 +1000

    add startsWith

--- index.ts
+++ index.ts
@@ -5,3 +5,5 @@ export const split = (char: string) => (string: string) => string.split(char);
 export const join = (char: string) => (strings: string[]) => strings.join(char);
 
 export const filterEmptyString = filter((a: string) => a.length > 0);
+
+export const startsWith = (a: string) => (b: string) => a.startsWith(b)
